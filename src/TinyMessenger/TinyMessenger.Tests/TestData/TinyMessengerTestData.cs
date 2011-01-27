859be3de4b2e7595265d1015a7ff0c61d4544d38
Steven Robbins <ste.robbins@gmail.com>
2011-01-27 21:27:23 +0000

    Initial push of separated TinyMessenger solution.
    Initially was part of the TinyIoC project, but separated it out as they're not really related.

---
+++ src/TinyMessenger/TinyMessenger.Tests/TestData/TinyMessengerTestData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TinyMessenger;
+
+namespace TinyMessenger.Tests.TestData
+{
+    public class TestMessage : TinyMessageBase
+    {
+        public TestMessage(object sender) : base(sender)
+        {
+            
+        }
+    }
+
+    public class TestProxy : ITinyMessageProxy
+    {
+        public ITinyMessage Message {get; private set;}
+
+        public void Deliver(ITinyMessage message, ITinyMessageSubscription subscription)
+        {
+            this.Message = message;
+            subscription.Deliver(message);
+        }
+    }
+
+}
---
+++ src/TinyMessenger/TinyMessenger.Tests/TestData/UtilityMethods.cs
@@ -0,0 +1,50 @@
+﻿//===============================================================================
+// TinyMessenger
+//
+// A simple messenger/event aggregator.
+//
+// https://github.com/grumpydev/TinyMessenger
+//===============================================================================
+// Copyright © Steven Robbins.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TinyMessenger;
+
+namespace TinyMessenger.Tests.TestData
+{
+    public class UtilityMethods
+    {
+        public static ITinyMessengerHub GetMessenger()
+        {
+            return new TinyMessengerHub();
+        }
+
+        public static void FakeDeliveryAction<T>(T message)
+            where T:ITinyMessage
+        {
+        }
+
+        public static bool FakeMessageFilter<T>(T message)
+            where T:ITinyMessage
+        {
+            return true;
+        }
+
+        public static TinyMessageSubscriptionToken GetTokenWithOutOfScopeMessenger()
+        {
+            var messenger = UtilityMethods.GetMessenger();
+
+            var token = new TinyMessageSubscriptionToken(messenger, typeof(TestMessage));
+
+            return token;
+        }
+    }
+}
